3c058e6b8d206a3991bc362a20d4ceaf6ab730ac
ASUS <azharasykari@gmail.com>
2024-08-28 09:44:32 +0700

    update file contact

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { 
+import {
     Box,
     Text,
     Heading,
     Container,
     Image
- } from "@chakra-ui/react";
+} from "@chakra-ui/react";
+
+export const Banner = () => {
+    return (
+      <Box bgImage="url('/image/banner/banner2.jpg')" pos="relative" bgRepeat="no-repeat" bgSize="cover" w="100%" h="45vh">
+        {/* Overlay */}
+        <Box pos="absolute" top="0" left="0" w="100%" h="100%" bg="black" opacity="0.3" zIndex="1"></Box>
+        <Box pos="relative" zIndex="10" h="100%" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+          <Heading textAlign="center" fontSize="65px" color="white" mb="0">
+            Contact us
+          </Heading>
+          {/* Breadcrumb */}
+          <Box display="flex" color="white" fontSize="20px" fontWeight="500" mt="2">
+            <Text>Home</Text>
+            <Text ml={2}>Contact us</Text>
+          </Box>
+        </Box>
+      </Box>
+    );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Heading, Container, Image, Text } from "@chakra-ui/react";
+import { Banner } from "../components/banner";
+const Contact = () => {
+  return (
+    <Box>
+      <Banner />
+      <Text>HALLO</Text>
+    </Box>
+  );
+};
+
+export default Contact;
