11a5fce93ecf0df88aa3eee7aa44bed3c10fca7f
ASUS <azharasykari@gmail.com>
2024-09-26 20:15:59 +0700

    updating routes

--- goserver/models/usermodel.go
+++ goserver/models/usermodel.go
@@ -18,8 +18,8 @@ type UserAuth struct {
 // GetUserByEmail mengambil data user berdasarkan email
 func GetUserByEmail(email string) (*UserAuth, error) {
     var user UserAuth
-    query := "SELECT id, username, firstname, lastname, email, password FROM authusers WHERE email = ?"
-    err := config.DB.QueryRow(query, email).Scan(&user.Id, &user.Username, &user.Firstname, &user.Lastname, &user.Email, &user.Password)
+    ExecuteQueryRow := "SELECT id, username, firstname, lastname, email, password FROM authusers WHERE email = ?"
+    err := config.DB.QueryRow(ExecuteQueryRow, email).Scan(&user.Id, &user.Username, &user.Firstname, &user.Lastname, &user.Email, &user.Password)
     if err != nil {
         return nil, err
     }
@@ -29,8 +29,8 @@ func GetUserByEmail(email string) (*UserAuth, error) {
 
 // CreateUser untuk menyimpan user baru di database
 func CreateUser(user *UserAuth) error {
-    query := "INSERT INTO authusers (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)"
-    _, err := config.DB.Exec(query, user.Username, user.Firstname, user.Lastname, user.Email, user.Password)
+    ExecuteQueryRow := "INSERT INTO authusers (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)"
+    _, err := config.DB.Exec(ExecuteQueryRow, user.Username, user.Firstname, user.Lastname, user.Email, user.Password)
     if err != nil {
         log.Println("Error membuat Data User:", err)
         return err
