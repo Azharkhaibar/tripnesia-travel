518382390557cbfb5f8e6ca88195c0a9db66d1a4
ASUS <azharasykari@gmail.com>
2024-09-26 20:13:00 +0700

    updating jwt

--- goserver/config/jwt.go
+++ goserver/config/jwt.go
@@ -1,4 +1,4 @@
 package config
 
-// JwtKey is the key used for signing JWT tokens
+
 var JwtKey = []byte("your_secret_key")
--- goserver/controllers/auth.go
+++ goserver/controllers/auth.go
@@ -20,19 +20,19 @@ type Claims struct {
 func Register(c *gin.Context) {
     var newUser models.UserAuth
     if err := c.ShouldBindJSON(&newUser); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Data Error"})
         return
     }
     // hash password
-    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+    hashPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
     if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal Hash Password"})
         return
     }
-    newUser.Password = string(hashedPassword)
+    newUser.Password = string(hashPassword)
     err = models.CreateUser(&newUser)
     if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menambahkan data User"})
         return
     }
 
@@ -61,7 +61,7 @@ func Login(c *gin.Context) {
     }
 
     // Buat token JWT
-    TimeExpiration := time.Now().Add(1 * time.Hour)
+    TimeExpiration := timeNow.Now().Add(1 * timeNow.Hour)
     claims := &Claims{
         Username: foundUser.Username,
         RegisteredClaims: jwt.RegisteredClaims{
